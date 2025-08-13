d4da1387caec980f22541cd6f12ac55c4f2272bd
amarojumahesh <amarojumahesh238@gmail.com>
2025-08-13 10:13:44 +0000

    these is readline-sync

---
+++ server/one.js
@@ -0,0 +1,7 @@
+import readline from "readline-sync";
+
+let age=readline.question("what is your age?");
+console.log(age);
+
+let fname = readline.question("what is your name?");
+console.log(fname);
